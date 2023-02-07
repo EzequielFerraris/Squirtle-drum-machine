3cdf8b2d2c386fb6a388874e7af98717efc9e356
Ezequiel Ferraris <Eze@MacBook-Air-de-Ezequiel.local>
2023-02-07 17:35:24 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DrumMachine from './components/DrumMachine.js';
+
+function App() {
+  return (
+    <div className="App">
+      <DrumMachine /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DrumMachine.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../stylesheets/DrumMachine.css';
+
+function DrumMachine() {
+
+    return (
+        <div className="main-container">
+
+          <button></button>  
+
+        </div>
+    );
+
+};
+
+export default DrumMachine;
